7d18fd631c1c06dfcc5ff523d8df7f045ec7e782
shaneemichael <shanemichaeltanata@gmail.com>
2024-09-30 21:53:30 +0700

    Finish Tugas 4

--- fresh_bite/settings.py
+++ fresh_bite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kg=j05@#&2912k#a$cqysjg111cin+-kmf60%2tw7id1jmq!fx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "shane-michael-freshbite2go.pbp.cs.ui.ac.id"]
 
